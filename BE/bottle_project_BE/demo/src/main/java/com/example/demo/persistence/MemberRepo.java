32d506d387ed556a39f0d29cf77824db8bf28b71
KSHNX2 <122770606+KSHNX2@users.noreply.github.com>
2023-05-04 18:56:05 +0900

    Delete BE/bottle_project_BE directory

--- BE/bottle_project_BE/demo/src/main/java/com/example/demo/persistence/MemberRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.domain.MemberVO;
-
-public interface MemberRepo extends CrudRepository<MemberVO, String> {
-
-}
