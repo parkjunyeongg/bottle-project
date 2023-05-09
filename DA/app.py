7cc64959a01dc291dd267bd745aeae41f2a255d3
KimSangWook57 <asdewrp@naver.com>
2023-05-09 16:53:58 +0900

    코드 수정

--- DA/app.py
+++ DA/app.py
@@ -25,7 +25,7 @@ def predict():
         return results.pandas().xyxy[0].to_json(orient='records')
 
 if __name__ == '__main__':
-    # model = torch.hub.load('ultralytics/yolov5', 'yolov5n', force_reload=True, skip_validation=True)
     model = yolov5.load('keremberke/yolov5m-garbage')
+    # model = torch.hub.load('ultralytics/yolov5', 'yolov5n', force_reload=True, skip_validation=True)
     app.run(host='0.0.0.0', debug=True, port=5000)
 
